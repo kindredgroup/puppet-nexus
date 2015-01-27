c8c02c63710efeab9f3cc0d51be9d510a53e0582
Johan Lyheden <Johan.Lyheden@UNIBET.com>
2015-01-27 13:23:10 +0100

    teardown on service

--- manifests/service.pp
+++ manifests/service.pp
@@ -4,9 +4,21 @@
 #
 class nexus::service {
 
+  case $::nexus::ensure {
+    present: {
+      $service_ensure = $::nexus::service_ensure
+      $service_enable = $::nexus::service_enable
+    }
+    absent: {
+      $service_ensure = 'stopped'
+      $service_enable = false
+    }
+    default: {}
+  }
+
   service { 'nexus':
-    ensure => $::nexus::service_ensure,
-    enable => $::nexus::service_enable
+    ensure => $service_ensure,
+    enable => $service_enable
   }
 
 }
